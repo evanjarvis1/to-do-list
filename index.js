40f49c6a5b80f29e00ad30f3c91b9da0206d5d9e
Evan Jarvis <evanlegee@gmail.com>
2023-03-15 17:39:09 -0600

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const newTaskForm = document.querySelector('form')
+
 let task1 = {name: 'Sweep the floor', priority: 'Low', status: false}
 
 let task2 = {name: 'Get groceries', priority: 'Medium', status: true}
@@ -6,75 +8,18 @@ let task3 = {name: 'Make dinner', priority: 'High', status: false}
 
 let tasks = [task1, task2, task3]
 
-// function addTask(task) {
-//   tasks.push(task)
-// }
+console.log(tasks)
 
-const addTask = task => {
-  tasks.push(task)
+const addTask = (event) => {
+    event.preventDefault()
+
+    const name = document.querySelector('#new-task-name')
+    const priority = document.querySelector('#new-task-priority')
+
+    const newTask = {name: name.value, priority: priority.value, status: false}
+
+    tasks.push(newTask)
+    console.log(tasks)
 }
 
-addTask({name: 'Take out the trash', priority: 'Medium', status: false})
-
-console.log(tasks)
-
-//write a function called deleteTask that takes a task name a parameter
-//loop over and remove the task if it is in the array of tasks
-//otherwise, log that task was not in the array
-
-// function deleteTask(task) {
-//   for(let i = 0; i < tasks.length; i++){
-//     if(task === tasks[i].name) {
-//       tasks.splice([i], 1)
-//       console.log('Task deleted')
-//       return
-//     }
-//   }
-
-//   console.log('That task was not in the array.')
-// }
-
-const deleteTask = (task) => {
-  for(let i = 0; i < tasks.length; i++){
-    if(task === tasks[i].name) {
-      tasks.splice([i], 1)
-      console.log('Task deleted')
-      return
-    }
-  }
-
-  console.log('That task was not in the array.')
-}
-
-deleteTask('Take out the trash')
-
-console.log(tasks)
-
-
-
-// const incompleteTasks = tasks.filter((task) => {
-//     if(!task.status){
-//         return true
-//     } else {
-//     return false
-//     }
-// })
-
-const incompleteTasks = tasks.filter((task) => !task.status)
-
-console.log(tasks)
-console.log(incompleteTasks)
-
-//write a for loop that will print each of the tasks in the tasks array
-//intermediate challenge: implement the above if statement into that for loop
-// for(let i = 0; i < tasks.length; i++) {
-//   let string = tasks[i]
-
-//   if(tasks[i] === 'Make dinner') {
-//     string += ' - Complete'
-//   } else {
-//     string += ' - Incomplete'
-//   }
-
-//   console.log(string)
-// }
+newTaskForm.addEventListener('submit', addTask)
